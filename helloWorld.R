78aaed10c7f47a0bfcf05a2f8562384e5952c624
RUSHI DAULATKAR <139751694+Rushi-Daulatkar@users.noreply.github.com>
2024-03-03 11:56:34 +0530

    Add files via upload

---
+++ helloWorld.R
@@ -0,0 +1,16 @@
+install.packages("rafalib")
+library(rafalib)
+
+install.packages("swirl")
+install.packages("swirl")
+
+# Create a numeric vector
+my_vector <- c(2.23, 3.45, 1.87, 2.11, 7.33, 18.34, 19.23)
+
+# Calculate the average
+average_value <- mean(my_vector)
+
+# Print the result
+print(average_value)
+
+
